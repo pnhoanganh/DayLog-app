6b1c7cdd02457361ddc43333228786781ac1ddf2
pnhoanganh <pnhoanganhh@gmail.com>
2025-06-06 23:08:54 +0700

    add: add animation package

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,22 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const {
+  wrapWithReanimatedMetroConfig,
+} = require("react-native-reanimated/metro-config");
 
-const config = getDefaultConfig(__dirname);
+let config = getDefaultConfig(__dirname);
 
-// Remove 'svg' from assetExts and add it to sourceExts
-config.transformer.babelTransformerPath = require.resolve(
-  "react-native-svg-transformer"
-);
-
+// Optional: SVG support
 config.resolver.assetExts = config.resolver.assetExts.filter(
   (ext) => ext !== "svg"
 );
 config.resolver.sourceExts.push("svg");
+config.transformer.babelTransformerPath = require.resolve(
+  "react-native-svg-transformer"
+);
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+// Gộp cấu hình
+config = withNativeWind(config, { input: "./global.css" });
+config = wrapWithReanimatedMetroConfig(config);
+
+module.exports = config;
