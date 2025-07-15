8c40e7807c8b63965dd7ccde825cad645478474b
pnhoanganh <pnhoanganhh@gmail.com>
2025-07-15 21:00:29 +0700

    feature: show report for a period of time instead  of showing all

--- app/tabs/Report.jsx
+++ app/tabs/Report.jsx
@@ -1,4 +1,4 @@
-import { View, FlatList } from "react-native";
+import { View, FlatList, ActivityIndicator } from "react-native";
 import { useEffect, useContext, useState } from "react";
 import { useNavigation } from "expo-router";
 import {
@@ -7,11 +7,13 @@ import {
 } from "react-native-responsive-screen";
 import { HabitContext } from "@/contexts/HabitContext";
 import HabitHistoryList from "@/components/Habit/HabitHistoryList";
-import EmptyState from "../../components/UI/EmptyState";
+import EmptyState from "@/components/UI/EmptyState";
 import { useFilter } from "@/contexts/FilterContext";
 import { Button } from "tamagui";
 import { FilterRemove } from "iconsax-react-nativejs";
 import COLORS from "@/constants/colors";
+import DropdownMenu from "@/components/UI/DropdownMenu";
+import dayjs from "dayjs";
 
 const Report = () => {
   const navigation = useNavigation();
@@ -19,6 +21,8 @@ const Report = () => {
   const [habitHistory, setHabitHistory] = useState([]);
   const [filteredHistory, setFilteredHistory] = useState([]);
   const [hasFiltered, setHasFiltered] = useState(false);
+  const [selectedRange, setSelectedRange] = useState(30);
+  const [isLoading, setIsLoading] = useState(false);
   const { selectedDates, applyFilter, setApplyFilter, setSelectedDates } =
     useFilter();
 
@@ -59,40 +63,92 @@ const Report = () => {
       .filter(Boolean);
   };
 
+  const filterLastNDays = (data, n) => {
+    const today = dayjs();
+    const cutoff = today.subtract(n - 1, "day");
+
+    return data
+      .map((monthData) => {
+        const filteredDays = monthData.days.filter((d) =>
+          dayjs(d.rawDate).isAfter(cutoff.subtract(1, "day"))
+        );
+
+        if (filteredDays.length === 0) return null;
+
+        return {
+          ...monthData,
+          days: filteredDays,
+          total: filteredDays.reduce((sum, d) => sum + d.count, 0),
+        };
+      })
+      .filter(Boolean);
+  };
+
   useEffect(() => {
     if (applyFilter) {
       const result = filterBySelectedDates(habitHistory, selectedDates);
       setFilteredHistory(result);
-      setApplyFilter(false); // reset flag
+      setApplyFilter(false);
       setHasFiltered(true);
     }
   }, [applyFilter]);
 
+  const ranges = [
+    { label: "7 days", days: 7 },
+    { label: "14 days", days: 14 },
+    { label: "1 month", days: 30 },
+    { label: "3 month", days: 90 },
+    { label: "Showing all check-ins", days: "total" },
+  ];
+
   const dataToRender =
     hasFiltered && Object.keys(selectedDates).length > 0
       ? filteredHistory
-      : habitHistory;
-
+      : selectedRange === "total"
+      ? habitHistory
+      : filterLastNDays(habitHistory, selectedRange);
   return (
-    <View style={{ flex: 1, backgroundColor: "#F2F1F5" }}>
-      <FlatList
-        data={dataToRender}
-        keyExtractor={(item) => item.month}
-        renderItem={({ item }) => (
-          <HabitHistoryList
-            month={item.month}
-            total={item.total}
-            days={item.days}
-            currentHabit={currentHabit}
-          />
-        )}
-        contentContainerStyle={{
-          paddingBottom: hp("20%"),
-          paddingHorizontal: wp("6%"),
-          paddingTop: hp("1%"),
+    <View style={{ backgroundColor: "#F2F1F5" }}>
+      <DropdownMenu
+        // label="Report for"
+        data={ranges.map((r) => ({ label: r.label, value: r.days }))}
+        color={currentHabit.color_code}
+        selectedValue={selectedRange}
+        onSelect={(item) => {
+          setIsLoading(true);
+          setSelectedRange(item.value);
+          setTimeout(() => {
+            setIsLoading(false);
+          }, 1000);
         }}
-        ListEmptyComponent={() => <EmptyState title="Oops! No checkins yet" />}
       />
+
+      {isLoading ? (
+        <View style={{ alignItems: "center", marginTop: hp("4%") }}>
+          <ActivityIndicator />
+        </View>
+      ) : (
+        <FlatList
+          data={dataToRender}
+          keyExtractor={(item) => item.month}
+          renderItem={({ item }) => (
+            <HabitHistoryList
+              month={item.month}
+              total={item.total}
+              days={item.days}
+              currentHabit={currentHabit}
+            />
+          )}
+          contentContainerStyle={{
+            paddingBottom: hp("20%"),
+            paddingHorizontal: wp("6%"),
+          }}
+          ListEmptyComponent={() => (
+            <EmptyState title="Oops! No checkins yet" />
+          )}
+        />
+      )}
+
       {Object.keys(selectedDates).length > 0 && (
         <Button
           themeInverse
